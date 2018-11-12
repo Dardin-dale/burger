339dcc9275b3769b3c92579b71767af7c8dd2d06
Dardin <logandalec@gmail.com>
2018-11-11 18:54:38 -0800

    yumm burgers

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name)
+VALUES('Baby you can chive my car burger');
+
+INSERT INTO burgers (burger_name)
+VALUES("Pepper don't preach burger");
+
+INSERT INTO burgers (burger_name)
+VALUES("Thank God it's Fried Egg burger");
+
+INSERT INTO burgers (burger_name)
+VALUES("Good Night and Good Leek burger");
